725d3c64033d4b0496c9647be814cbf0e7abe055
WangSsuYen <a0909321155@icloud.com>
2023-10-29 17:29:49 +0800

    '修改註冊程式內容'

--- app/unit.py
+++ app/unit.py
@@ -6,8 +6,37 @@ import re
 
 
 class DataSet():
+    def __init__(self, uniform_numbers, client_password, password_confirm):
+        self.uniform_numbers = uniform_numbers
+        self.client_password = client_password
+        self.password_confirm = password_confirm
+
     def check_user_login(request):
         user = request.session.get('uniform_numbers')
         if user is None:
             return None  # 使用 None 表示未登入
         return user  # 返回已登入的使用者名稱
+
+    def check_signup(self):
+        cursor = connection.cursor()
+        cursor.execute(
+            "SELECT * FROM client WHERE uniform_numbers = %s", (self.uniform_numbers, ))
+        # 因%s為傳輸文字，因此uniform_numbers須轉為Str
+        user = cursor.fetchone()
+        if user is not None:
+            return "帳號已存在，請重新輸入"
+
+        if self.client_password != self.password_confirm:
+            return "密碼不相符，請重新輸入。"
+
+        if self.client_password is None or self.password_confirm is None or self.uniform_numbers is None:
+            return "帳號或密碼不可為空，請重新輸入。"
+
+        if len(self.client_password) < 6 or len(self.client_password) > 15:
+            return "密碼格式錯誤，密碼長度請設定6到15位。"
+
+        if not re.search(r'[a-zA-Z]', self.client_password):
+            return "密碼必須包含至少一個英文字母。"
+
+        if len(self.uniform_numbers) != 8:
+            return "統一編號必須為8個數字"
--- app/views.py
+++ app/views.py
@@ -41,33 +41,19 @@ class Client():
             creattime = timezone.now().strftime("%Y-%m-%d %H:%M:%S")
             # print(uniform_numbers, client_password,password_confirm, creattime)
 
-            if client_password != password_confirm:
-                errmesg = "密碼不相符，請重新輸入。"
-                return render(request, 'client_base.html', {'errmesg': errmesg})
-
-            if client_password is None or password_confirm is None or uniform_numbers is None:
-                errmesg = "帳號或密碼不可為空，請重新輸入。"
-                return render(request, 'client_base.html', {'errmesg': errmesg})
-
-            if len(client_password) < 6 or len(client_password) > 15:
-                errmesg = "密碼格式錯誤，密碼長度請設定6到15位。"
-                return render(request, 'client_base.html', {'errmesg': errmesg})
-
-            if not re.search(r'[a-zA-Z]', client_password):
-                errmesg = "密碼必須包含至少一個英文字母。"
-                return render(request, 'client_base.html', {'errmesg': errmesg})
-
-            if len(uniform_numbers) != 8:
-                errmesg = "統一編號必須為8個數字"
-                return render(request, 'client_base.html', {'errmesg': errmesg})
-
-            try:
-                cursor = connection.cursor()
-                cursor.execute("INSERT INTO client (uniform_numbers , client_password , creattime) VALUES (%s , %s , %s)", (
-                    uniform_numbers, client_password, creattime))
-                return render(request, 'client_base.html', {'succmesg': '註冊成功，請重新登入'})
-            except Exception as err:
-                return render(request, 'client_base.html', {'errmesg': err})
+            validator = DataSet(
+                uniform_numbers, client_password, password_confirm)
+            errmesg = validator.check_signup()
+            if errmesg is not None:
+                return render(request, "client_base.html", {'errmesg': errmesg})
+            else:
+                try:
+                    cursor = connection.cursor()
+                    cursor.execute("INSERT INTO client (uniform_numbers , client_password , creattime) VALUES (%s , %s , %s)", (
+                        uniform_numbers, client_password, creattime))
+                    return render(request, 'client_base.html', {'succmesg': '註冊成功，請重新登入'})
+                except Exception as err:
+                    return render(request, 'client_base.html', {'errmesg': err})
 
         return render(request, 'client_base.html')
 
